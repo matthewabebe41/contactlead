b28edee615c0e6b59812867951bcc76c52248ff1
Matthew <matthewabebe41@gmail.com>
2025-06-09 19:10:17 -0400

    testing serve css file

--- server/index.js
+++ server/index.js
@@ -21,6 +21,7 @@ app.use(express.json({
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "../public/css/index.css")));
 
 app.get("/contacts", (req, res) => {
     res.sendFile(path.join(__dirname, "../index.html"));
