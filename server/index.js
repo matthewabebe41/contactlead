358b0498304acad45f3cc8b01ac027369ac8f25c
Matthew <matthewabebe41@gmail.com>
2025-06-11 00:36:48 -0400

    added route for root path

--- server/index.js
+++ server/index.js
@@ -23,6 +23,9 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "../public")));
 // app.use('css', express.static(path.join(__dirname, "/public/css")));
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../index.html"));
+});
 app.get("/contacts", (req, res) => {
     res.sendFile(path.join(__dirname, "../index.html"));
 });
