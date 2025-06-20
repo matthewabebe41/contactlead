0d12fd9875095a64e34feea7cedae7d0f73aabde
Matthew <matthewabebe41@gmail.com>
2025-06-20 06:52:54 -0400

    change postgres config

--- server/index.js
+++ server/index.js
@@ -7,7 +7,9 @@ const path = require("path");
 
 const pool = new pg.Pool({
    connectionString: process.env.DATABASE_URL,
-    ssl: process.env.DATABASE_URL ? true : false
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 app.use(express.json({
