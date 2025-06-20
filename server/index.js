277e8a97faa0182dbb42373ef49a520c235c30db
Matthew <matthewabebe41@gmail.com>
2025-06-20 01:04:03 -0400

    test config for postgres on heroku

--- server/index.js
+++ server/index.js
@@ -7,15 +7,15 @@ const path = require("path");
 const multer = require("multer");
 
 const pool = new pg.Pool({
-    // connectionString: process.env.DATABASE_URL,
-    // ssl: {
-    //     rejectUnauthorized: false,
-    // },
-    user: "postgres",
-    password: "password",
-    host: "127.0.0.1",
-    port: 5432,
-    database: "contactlead"
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+    // user: "postgres",
+    // password: "password",
+    // host: "127.0.0.1",
+    // port: 5432,
+    // database: "contactlead"
 });
 
 app.use(express.json({
